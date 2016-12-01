db46b6cb5ee09b9dfd0c0577618dacd42626ee2d
Adib Hanna <adibhanna@gmail.com>
2016-11-30 23:01:13 -0800

    do

--- resources/views/pages/partials/footer.blade.php
+++ resources/views/pages/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="row">
         <div class="col-md-6">
-            <p>Copyright © {{ date("Y") }} - Adib Hanna</p>
+            <p>Proudly hosted on <a href="https://m.do.co/c/7f4236088cc3" target="_blank">Digital Ocean</a></p>
         </div>
         <div class="col-md-6">
             <div class="contact text-right">
